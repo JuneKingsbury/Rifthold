b89d2b635b8395d5cd2eb9d56a7263f203decd59
jurbani <jurbanics@gmail.com>
2026-08-19 14:33:39 -0400

    Updated changelog wording

--- js/ui/changelog.js
+++ js/ui/changelog.js
@@ -162,6 +162,7 @@ export function renderCreditsHTML() {
     <div style="color:#aaa; margin-bottom:4px;"><a style="color:#aaa;" href="https://bdragon1727.itch.io/free-smoke-fx-pixel-2">Free Smoke Fx Pixel 2</a> by BDragon1727</div>
     <div style="color:#ccc; margin-bottom:8px;"><b>Music & SFX</b></div>
     <div style="color:#aaa; margin-bottom:4px;"><a style="color:#aaa;" href="https://rapidpunches.itch.io/game-of-the-unknown">GAME OF THE UNKNOWN</a> by RAPIDPUNCHES</div>
+    <div style="color:#aaa; margin-bottom:4px;"><a style="color:#aaa;" href="https://www.bfxr.net/"bfxr (custom SFX maker)</a> by increpare</div>
     <div style="color:#666; font-size:10px; margin-top:20px;">Version 0.2 — Aug 2026</div>
 </div>`;
 }
